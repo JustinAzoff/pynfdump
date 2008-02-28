75d90bde8158ffe8e5f82e1201fa7921393d55d5
Justin Azoff <justin@bouncybouncy.net>
2008-02-28 20:56:32 +0000

    fix typo again in import

--- setup.py
+++ setup.py
@@ -5,21 +5,20 @@ from glob import glob
 version = '0.1'
 
 setup(name='pynfdump',
-      version=version,
-      description="python interface to nfdump",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='nfdump netflow',
-      author='Justin Azoff',
-      author_email='JAzoff@uamail.albany.edu',
-      url='',
-      license='LGPL',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
+    version=version,
+    description="python interface to nfdump",
+    long_description="""\ """,
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='nfdump netflow',
+    author='Justin Azoff',
+    author_email='JAzoff@uamail.albany.edu',
+    url='',
+    license='LGPL',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
         "dateutil"
-      ],
-      scripts=glob('scripts/*'),
-      )
+    ],
+    scripts=glob('scripts/*'),
+    )
